0a27a10835e5b333f957cc295df30cde91678762
한국 <koreahghg@gmail.com>
2025-09-25 23:06:39 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(){
+    int a, b;
+    while(1){
+        scanf("%d %d", &a, &b);
+        if(a+b == 0){
+            return 0;
+        }
+        printf("%d\n", a+b);
+    }
+}
