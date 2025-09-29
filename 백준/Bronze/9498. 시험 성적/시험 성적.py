aa5f3854b3c2f0185f46a729bb42d80c0f2ad961
한국 <koreahghg@gmail.com>
2025-09-30 07:58:13 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+a = int(input())
+
+if a // 10 == 10 or a // 10 == 9:
+    print("A")
+elif a // 10 == 8:
+    print("B")
+elif a // 10 == 7:
+    print("C")
+elif a // 10 == 6:
+    print("D")
+else:
+    print("F")
