da41d1283eeb65627ee628398c5e6f3bdfae6de5
한국 <koreahghg@gmail.com>
2026-01-12 21:16:13 +0900

    [level 0] Title: 배열 만들기 1, Time: 5.91 ms, Memory: 37.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.js
@@ -0,0 +1,10 @@
+function solution(n, k) {
+    let a = [];
+    for(let i = 1; i<=n; i++){
+        if(i%k == 0){
+            a.push(i);
+        }
+    }
+    a.sort((a, b) => a - b);
+    return a; 
+}
