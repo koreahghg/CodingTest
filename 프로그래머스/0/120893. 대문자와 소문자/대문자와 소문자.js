c66a3730e4ed0bb60e53f6a9b3e501e3ef17f9f5
한국 <koreahghg@gmail.com>
2026-02-10 23:21:26 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.41 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -0,0 +1,13 @@
+function solution(my_string) {
+  let result = '';
+  
+  for (let char of my_string) {
+    if (char === char.toLowerCase()) {
+      result += char.toUpperCase();
+    } else {
+      result += char.toLowerCase();
+    }
+  }
+  
+  return result;
+}
