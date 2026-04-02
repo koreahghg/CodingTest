e25a3281c5d682092496c0aec50e36552cc166c8
한국 <koreahghg@gmail.com>
2026-04-02 21:03:12 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 38.49 ms, Memory: 116 MB -BaekjoonHub

---
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -0,0 +1,12 @@
+function solution(arr) {
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] >= 50 && arr[i] % 2 == 0){
+            arr[i] /= 2;
+        }
+        else if(arr[i]<50 && arr[i] % 2 != 0){
+            arr[i] *= 2;
+        }
+        else continue;
+    }
+    return arr;
+}
