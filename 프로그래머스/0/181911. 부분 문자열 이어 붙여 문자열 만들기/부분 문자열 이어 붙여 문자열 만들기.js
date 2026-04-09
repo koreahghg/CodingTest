76145356220ffcc45df4eedf0fbed099de9260c4
한국 <koreahghg@gmail.com>
2026-04-09 23:59:39 +0900

    [level 0] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 0.21 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.js
@@ -0,0 +1,8 @@
+function solution(my_strings, parts) {
+    return my_strings
+        .map((str, i) => {
+            const [s, e] = parts[i];
+            return str.slice(s, e + 1);
+        })
+        .join('');
+}
